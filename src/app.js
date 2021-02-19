c34621341accf5afe74d7a46aeba82fe6d5a90d0
Sijuade Ajagunna <mahyor.sam@gmail.com>
2021-02-19 17:49:16 +0100

    add pug example

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (request, response) => {
   return response.send('OK');
+});
+
+app.get('/index', (request, response) => {
+  response.render('index', {
+    subject: 'Pug template engine',
+    name: 'our template',
+    link: 'https://google.com',
+  });
 });
 
 app.listen(5000, () => {
