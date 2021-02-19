3c2954b311869dd38df3fa810d8b822eebb83caf
Sijuade Ajagunna <mahyor.sam@gmail.com>
2021-02-19 21:48:02 +0100

    ejs template

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (request, response) => {
   return response.send('OK');
+});
+
+app.get('/index', (request, response) => {
+  response.render('index', {
+    subject: 'EJS template engine',
+    name: 'our template',
+    link: 'https://google.com',
+  });
 });
 
 app.listen(5000, () => {
