a41c4c19de7335c3914aaeafdc64e8afe7814f77
Sijuade Ajagunna <mahyor.sam@gmail.com>
2021-02-21 11:26:59 +0100

    set up hbs template

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
 const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.get('/', (request, response) => {
   return response.send('OK');
+});
+
+app.get('/index', (request, response) => {
+  response.render('index', {
+    subject: 'hbs template engine',
+    name: 'our template',
+    link: 'https://google.com'
+  });
 });
 
 app.listen(5000, () => {
